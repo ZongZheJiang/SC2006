56b4bb8f7606cded0cd751678da4aca234073b08
ZongZheJiang <jiangzongzhe374@gmail.com>
2024-10-17 02:09:28 +0800

    Cleaned

--- backend/api.py
+++ backend/api.py
@@ -30,23 +30,14 @@ def get_token():
     else:
         return None
 
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
 @app.route("/get_carpark_nameToNumber")
 def get_carpark_nameToNumber():
     carpark_nameToNumber = {}
     csv_file = 'HDBCarparkInformation.csv'
     
     with open(csv_file, mode='r') as file:
-        # Create a CSV reader that returns rows as dictionaries
         reader = csv.DictReader(file)
-
-        # Iterate through each row and store it in the dictionary
         for row in reader:
-            # Use Carpark_Name as the key and the rest of the row as the value
             carpark_name = row['address']
             carpark_nameToNumber[carpark_name] = {
                 'carpark_number': row['car_park_no']
@@ -59,12 +50,8 @@ def get_carpark_nameToLongLat():
     csv_file = 'HDBCarparkInformation.csv'
     
     with open(csv_file, mode='r') as file:
-        # Create a CSV reader that returns rows as dictionaries
         reader = csv.DictReader(file)
-
-        # Iterate through each row and store it in the dictionary
         for row in reader:
-            # Use Carpark_Name as the key and the rest of the row as the value
             carpark_name = row['address']
             carpark_nameToLongLat[carpark_name] = {
                 'long': row['long'],
@@ -74,17 +61,12 @@ def get_carpark_nameToLongLat():
 
 @app.route('/carpark-availability')
 def get_carpark_availability():
-    # API endpoint
     url = "https://api.data.gov.sg/v1/transport/carpark-availability"
     
     try:
-        # Sending GET request to the API
         response = requests.get(url)
-        # Checking if the request was successful
         response.raise_for_status()
-        # Parse the response to JSON format
         data = response.json()
-        # Returning the data as JSON
         return jsonify(data)
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
@@ -107,5 +89,3 @@ def get_carpark_rates():
         return jsonify(data['Result'])
     else:
         return jsonify({'error': 'Failed to fetch car park rates'}), 500
-    
-
